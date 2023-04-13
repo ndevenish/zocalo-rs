cfd6589d6c20ec568c1715d98593c28a41f14be9
Nicholas Devenish <ndevenish@gmail.com>
2023-04-13 11:56:45 +0100

    First pass at DAG cycle detection

--- src/workflows/lib.rs
+++ src/workflows/lib.rs
@@ -1,4 +1,7 @@
-use std::collections::HashMap;
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::Display,
+};
 
 use serde::{Deserialize, Serialize};
 
@@ -8,11 +11,19 @@ use serde::{Deserialize, Serialize};
 //     Lookup(HashMap<String, i64>),
 // }
 
+type NodeID = i32;
+
 #[derive(Serialize, Deserialize, Debug)]
 // #[serde(deny_unknown_fields)]
 pub struct Node {
     queue: String,
     service: String,
+}
+
+impl Node {
+    pub fn all_outgoing(&self) -> HashSet<NodeID> {
+        HashSet::new()
+    }
 }
 
 #[serde_with::serde_as]
@@ -21,12 +32,79 @@ pub struct Recipe {
     /// Recipe steps
     #[serde(flatten)]
     #[serde_as(as = "HashMap<serde_with::DisplayFromStr, _>")]
-    nodes: HashMap<i32, Node>,
+    nodes: HashMap<NodeID, Node>,
     /// The list of all nodes to start the graph processing, with optional initial parameters.
-    start: Vec<(i32, Option<serde_json::Value>)>,
+    start: Vec<(NodeID, Option<serde_json::Value>)>,
     /// Nodes to trigger when an error happens
     #[serde(default)]
-    error: Vec<i32>,
+    error: Vec<NodeID>,
+}
+
+#[derive(PartialEq, Hash, Eq, Copy, Clone)]
+enum NodeVertex {
+    Start,
+    Error,
+    Node(NodeID),
+}
+
+impl Display for NodeVertex {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            NodeVertex::Start => write!(f, "start"),
+            NodeVertex::Error => write!(f, "end"),
+            NodeVertex::Node(x) => write!(f, "{x}"),
+        }
+    }
+}
+
+/// Generate a list of reachable nodes from a particular recipe vertex
+///
+/// Will generate an error if the graph is not a DAG (has cycles)
+fn all_reachable_dag_nodes(recipe: &Recipe, start: NodeVertex) -> Result<HashSet<NodeID>, String> {
+    let mut all_nodes = HashSet::new();
+    let mut current_path = vec![start];
+
+    // let mut current_node = start;
+    loop {
+        let current_node = *current_path.last().unwrap();
+        all_nodes.insert(current_node.to_owned());
+
+        // Get a list of all nodes going out from this one
+        let outgoing_nodes: HashSet<i32> = match current_node {
+            NodeVertex::Start => recipe.start.iter().map(|x| x.0).collect(),
+            NodeVertex::Error => recipe.error.iter().copied().collect(),
+            NodeVertex::Node(id) => recipe
+                .nodes
+                .get(&id)
+                .ok_or(format!("Referenced node {id} is missing from recipe"))?
+                .all_outgoing(),
+        };
+        // If any of these nodes are in our current path, then we've detected a cycle
+        let current_path_set: HashSet<NodeVertex> = current_path.into_iter().collect();
+        let cycle_nodes = outgoing_nodes
+            .iter()
+            .map(|x| NodeVertex::Node(*x))
+            .collect::<HashSet<NodeVertex>>()
+            .intersection(&current_path_set)
+            .cloned()
+            .collect::<Vec<NodeVertex>>();
+        if !cycle_nodes.is_empty() {
+            Err(format!(
+                "Cycle in DAG detected from node {current_node} → {}",
+                cycle_nodes.first().unwrap()
+            ))?;
+        }
+
+        break;
+    }
+    // Extract the numeric nodes only to return
+    Ok(all_nodes
+        .iter()
+        .filter_map(|&x| match x {
+            NodeVertex::Node(id) => Some(id),
+            _ => None,
+        })
+        .collect())
 }
 
 impl Recipe {
