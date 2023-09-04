b25da61ba78b3e2994cad999c12cd0a75ae99d84
Nicholas Devenish <ndevenish@gmail.com>
2023-09-04 22:12:45 +0100

    Include a test RecipeWrapper message

--- src/workflows/recipe_wrapper.rs
+++ src/workflows/recipe_wrapper.rs
@@ -3,12 +3,6 @@ use serde::{Deserialize, Serialize};
 use crate::{NodeID, Recipe};
 
 /// An in-progress workflow
-///
-/// Contains the recipe for the workflow, along with:
-/// - A marker of where the workflow currently is
-/// - The path it has taken to get here
-/// - The payload associated with the message e.g. what the last service
-///   sent (presumably to the currently-running service)
 #[derive(Serialize, Deserialize, Debug)]
 pub struct RecipeWrapper {
     /// The current in-progress recipe
@@ -21,6 +15,70 @@ pub struct RecipeWrapper {
     pub recipe_path: Vec<NodeID>,
     /// Recipe-wide key-value configurations
     pub environment: serde_json::Map<String, serde_json::Value>,
-    /// The message content for this node of the recipe
+    /// The message content for the targeted node of the recipe
     pub payload: serde_json::Value,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::RecipeWrapper;
+
+    const RECIPE_WRAPPER: &str = r#"    {
+        "recipe": {
+            "1": {
+                "service": "A service",
+                "queue": "some.queue.{first}",
+                "output": [
+                    "2"
+                ],
+                "error": 2
+            },
+            "2": {
+                "service": "B service",
+                "queue": "another.queue.{name}",
+                "output": {
+                    "all": "3"
+                }
+            },
+            "3": {
+                "service": "C Service",
+                "queue": "third"
+            },
+            "start": [
+                [
+                    1,
+                    {}
+                ]
+            ],
+            "error": [
+                2
+            ]
+        },
+        "environment": {
+            "ID": "fff0b833-a379-4ca5-b102-61ae87958393",
+            "ispyb_autoproc_id": 13785026,
+            "ispyb_autoprocprogram_id": 97260023,
+            "ispyb_autoprocscaling_id": 13784840,
+            "ispyb_integration_id": 19771526
+        },
+        "payload": {
+            "result": [
+                {
+                    "id": 97260023,
+                    "jobId": 17598926,
+                    "message": "processing successful",
+                    "programs": "xia2.multiplex",
+                    "status": 1
+                }
+            ]
+        },
+        "recipe-path": [],
+        "recipe-pointer": 1
+    }"#;
+
+    #[test]
+    fn read_test_wrapper() {
+        let rw: RecipeWrapper = serde_json::from_str(RECIPE_WRAPPER).unwrap();
+        println!("{rw:#?}");
+    }
 }
